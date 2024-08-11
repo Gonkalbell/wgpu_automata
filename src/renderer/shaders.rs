52b62848b7fbff28d79961c48133fc3d85dd92e2
JM Bell <johnmichaelbell@gmail.com>
2024-08-11 16:15:16 -0400

    reformated shaders.rs

--- src/renderer/shaders.rs
+++ src/renderer/shaders.rs
@@ -15,10 +15,7 @@ impl ShaderEntry {
             Self::Skybox => skybox::create_pipeline_layout(device),
         }
     }
-    pub fn create_shader_module_embed_source(
-        &self,
-        device: &wgpu::Device,
-    ) -> wgpu::ShaderModule {
+    pub fn create_shader_module_embed_source(&self, device: &wgpu::Device) -> wgpu::ShaderModule {
         match self {
             Self::Skybox => skybox::create_shader_module_embed_source(device),
         }
@@ -30,21 +27,21 @@ mod _root {
 pub mod layout_asserts {
     use super::{_root, _root::*};
     const WGSL_BASE_TYPE_ASSERTS: () = {
-        assert!(std::mem::size_of:: < glam::Vec3A > () == 16);
-        assert!(std::mem::align_of:: < glam::Vec3A > () == 16);
-        assert!(std::mem::size_of:: < glam::Vec4 > () == 16);
-        assert!(std::mem::align_of:: < glam::Vec4 > () == 16);
-        assert!(std::mem::size_of:: < glam::Mat3A > () == 48);
-        assert!(std::mem::align_of:: < glam::Mat3A > () == 16);
-        assert!(std::mem::size_of:: < glam::Mat4 > () == 64);
-        assert!(std::mem::align_of:: < glam::Mat4 > () == 16);
+        assert!(std::mem::size_of::<glam::Vec3A>() == 16);
+        assert!(std::mem::align_of::<glam::Vec3A>() == 16);
+        assert!(std::mem::size_of::<glam::Vec4>() == 16);
+        assert!(std::mem::align_of::<glam::Vec4>() == 16);
+        assert!(std::mem::size_of::<glam::Mat3A>() == 48);
+        assert!(std::mem::align_of::<glam::Mat3A>() == 16);
+        assert!(std::mem::size_of::<glam::Mat4>() == 64);
+        assert!(std::mem::align_of::<glam::Mat4>() == 16);
     };
     const BGROUP_CAMERA_CAMERA_ASSERTS: () = {
         assert!(std::mem::offset_of!(bgroup_camera::Camera, view) == 0);
         assert!(std::mem::offset_of!(bgroup_camera::Camera, view_inv) == 64);
         assert!(std::mem::offset_of!(bgroup_camera::Camera, proj) == 128);
         assert!(std::mem::offset_of!(bgroup_camera::Camera, proj_inv) == 192);
-        assert!(std::mem::size_of:: < bgroup_camera::Camera > () == 256);
+        assert!(std::mem::size_of::<bgroup_camera::Camera>() == 256);
     };
 }
 pub mod bgroup_camera {
@@ -111,41 +108,38 @@ pub mod skybox {
     #[derive(Debug)]
     pub struct WgpuBindGroup0(wgpu::BindGroup);
     impl WgpuBindGroup0 {
-        pub const LAYOUT_DESCRIPTOR: wgpu::BindGroupLayoutDescriptor<'static> = wgpu::BindGroupLayoutDescriptor {
-            label: Some("Skybox::BindGroup0::LayoutDescriptor"),
-            entries: &[
-                /// @binding(0): "_root::bgroup_camera::res_camera"
-                wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
-                    ty: wgpu::BindingType::Buffer {
-                        ty: wgpu::BufferBindingType::Uniform,
-                        has_dynamic_offset: false,
-                        min_binding_size: std::num::NonZeroU64::new(
-                            std::mem::size_of::<_root::bgroup_camera::Camera>() as _,
-                        ),
+        pub const LAYOUT_DESCRIPTOR: wgpu::BindGroupLayoutDescriptor<'static> =
+            wgpu::BindGroupLayoutDescriptor {
+                label: Some("Skybox::BindGroup0::LayoutDescriptor"),
+                entries: &[
+                    /// @binding(0): "_root::bgroup_camera::res_camera"
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                        ty: wgpu::BindingType::Buffer {
+                            ty: wgpu::BufferBindingType::Uniform,
+                            has_dynamic_offset: false,
+                            min_binding_size: std::num::NonZeroU64::new(std::mem::size_of::<
+                                _root::bgroup_camera::Camera,
+                            >(
+                            )
+                                as _),
+                        },
+                        count: None,
                     },
-                    count: None,
-                },
-            ],
-        };
+                ],
+            };
         pub fn get_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
             device.create_bind_group_layout(&Self::LAYOUT_DESCRIPTOR)
         }
-        pub fn from_bindings(
-            device: &wgpu::Device,
-            bindings: WgpuBindGroup0Entries,
-        ) -> Self {
+        pub fn from_bindings(device: &wgpu::Device, bindings: WgpuBindGroup0Entries) -> Self {
             let bind_group_layout = Self::get_bind_group_layout(&device);
             let entries = bindings.as_array();
-            let bind_group = device
-                .create_bind_group(
-                    &wgpu::BindGroupDescriptor {
-                        label: Some("Skybox::BindGroup0"),
-                        layout: &bind_group_layout,
-                        entries: &entries,
-                    },
-                );
+            let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: Some("Skybox::BindGroup0"),
+                layout: &bind_group_layout,
+                entries: &entries,
+            });
             Self(bind_group)
         }
         pub fn set<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
@@ -185,48 +179,41 @@ pub mod skybox {
     #[derive(Debug)]
     pub struct WgpuBindGroup1(wgpu::BindGroup);
     impl WgpuBindGroup1 {
-        pub const LAYOUT_DESCRIPTOR: wgpu::BindGroupLayoutDescriptor<'static> = wgpu::BindGroupLayoutDescriptor {
-            label: Some("Skybox::BindGroup1::LayoutDescriptor"),
-            entries: &[
-                /// @binding(0): "res_texture"
-                wgpu::BindGroupLayoutEntry {
-                    binding: 0,
-                    visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
-                    ty: wgpu::BindingType::Texture {
-                        sample_type: wgpu::TextureSampleType::Float {
-                            filterable: true,
+        pub const LAYOUT_DESCRIPTOR: wgpu::BindGroupLayoutDescriptor<'static> =
+            wgpu::BindGroupLayoutDescriptor {
+                label: Some("Skybox::BindGroup1::LayoutDescriptor"),
+                entries: &[
+                    /// @binding(0): "res_texture"
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 0,
+                        visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                        ty: wgpu::BindingType::Texture {
+                            sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                            view_dimension: wgpu::TextureViewDimension::Cube,
+                            multisampled: false,
                         },
-                        view_dimension: wgpu::TextureViewDimension::Cube,
-                        multisampled: false,
+                        count: None,
                     },
-                    count: None,
-                },
-                /// @binding(1): "res_sampler"
-                wgpu::BindGroupLayoutEntry {
-                    binding: 1,
-                    visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
-                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
-                    count: None,
-                },
-            ],
-        };
+                    /// @binding(1): "res_sampler"
+                    wgpu::BindGroupLayoutEntry {
+                        binding: 1,
+                        visibility: wgpu::ShaderStages::VERTEX_FRAGMENT,
+                        ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                        count: None,
+                    },
+                ],
+            };
         pub fn get_bind_group_layout(device: &wgpu::Device) -> wgpu::BindGroupLayout {
             device.create_bind_group_layout(&Self::LAYOUT_DESCRIPTOR)
         }
-        pub fn from_bindings(
-            device: &wgpu::Device,
-            bindings: WgpuBindGroup1Entries,
-        ) -> Self {
+        pub fn from_bindings(device: &wgpu::Device, bindings: WgpuBindGroup1Entries) -> Self {
             let bind_group_layout = Self::get_bind_group_layout(&device);
             let entries = bindings.as_array();
-            let bind_group = device
-                .create_bind_group(
-                    &wgpu::BindGroupDescriptor {
-                        label: Some("Skybox::BindGroup1"),
-                        layout: &bind_group_layout,
-                        entries: &entries,
-                    },
-                );
+            let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+                label: Some("Skybox::BindGroup1"),
+                layout: &bind_group_layout,
+                entries: &entries,
+            });
             Self(bind_group)
         }
         pub fn set<'a>(&'a self, render_pass: &mut wgpu::RenderPass<'a>) {
@@ -301,9 +288,7 @@ pub mod skybox {
             },
         }
     }
-    pub fn fs_skybox_entry(
-        targets: [Option<wgpu::ColorTargetState>; 1],
-    ) -> FragmentEntry<1> {
+    pub fn fs_skybox_entry(targets: [Option<wgpu::ColorTargetState>; 1]) -> FragmentEntry<1> {
         FragmentEntry {
             entry_point: ENTRY_FS_SKYBOX,
             targets,
@@ -320,27 +305,21 @@ pub mod skybox {
         }
     }
     pub fn create_pipeline_layout(device: &wgpu::Device) -> wgpu::PipelineLayout {
-        device
-            .create_pipeline_layout(
-                &wgpu::PipelineLayoutDescriptor {
-                    label: Some("Skybox::PipelineLayout"),
-                    bind_group_layouts: &[
-                        &WgpuBindGroup0::get_bind_group_layout(device),
-                        &WgpuBindGroup1::get_bind_group_layout(device),
-                    ],
-                    push_constant_ranges: &[],
-                },
-            )
+        device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+            label: Some("Skybox::PipelineLayout"),
+            bind_group_layouts: &[
+                &WgpuBindGroup0::get_bind_group_layout(device),
+                &WgpuBindGroup1::get_bind_group_layout(device),
+            ],
+            push_constant_ranges: &[],
+        })
     }
-    pub fn create_shader_module_embed_source(
-        device: &wgpu::Device,
-    ) -> wgpu::ShaderModule {
+    pub fn create_shader_module_embed_source(device: &wgpu::Device) -> wgpu::ShaderModule {
         let source = std::borrow::Cow::Borrowed(SHADER_STRING);
-        device
-            .create_shader_module(wgpu::ShaderModuleDescriptor {
-                label: Some("skybox.wgsl"),
-                source: wgpu::ShaderSource::Wgsl(source),
-            })
+        device.create_shader_module(wgpu::ShaderModuleDescriptor {
+            label: Some("skybox.wgsl"),
+            source: wgpu::ShaderSource::Wgsl(source),
+        })
     }
     pub const SHADER_STRING: &'static str = r#"
 struct CameraX_naga_oil_mod_XMJTXE33VOBPWGYLNMVZGCX {
